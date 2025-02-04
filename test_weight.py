d7b39fc856e0a0f6756f9a5cf7dfe90a43eb811e
huyquoctrinh <trnhquchuy@yahoo.com.vn>
2025-02-05 01:35:44 +0200

    update the open-source

--- test_weight.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-state_dict = torch.load("/workspace/continuation/test/open-singsong/results/llambada_semantic/llambada.transformer.77000.pt")
-print(state_dict.keys())
-print(state_dict["transformer.layers.0.0.to_out.0.weight"].shape)
